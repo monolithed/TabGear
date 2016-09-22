6a1e46ae71ee7339214dd8479a48f26e5e948620
Alexander Abashkin <monolithed@gmail.com>
2016-09-22 20:52:10 +0300

    ITEMS_FAILED -> ITEMS_LOADED

--- views/middleware/api.js
+++ views/middleware/api.js
@@ -2,5 +2,5 @@ import { items } from '../stabs';
 import * as ActionTypes from '../actions';
 
 export default store => next => action => {
-	next({ type: ActionTypes.ITEMS_FAILED, items, error: 'FAIL' });
+	next({ type: ActionTypes.ITEMS_LOADED, items, error: null });
 };
