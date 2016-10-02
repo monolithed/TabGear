402ac6cbd125793034f582c17351adf7cc943f89
Alexander Abashkin <monolithed@gmail.com>
2016-10-02 22:09:49 +0300

    added search

---
+++ views/components/Search/index.jsx
@@ -0,0 +1,12 @@
+import React, { Component, PropTypes } from 'react';
+import './index.css';
+
+class Search extends Component {
+	render () {
+		return <div> <input type="text" placeholder="Search tab..." /> </div>;
+	}
+}
+
+Search.propTypes = {};
+
+export default Search;
