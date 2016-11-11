417631cdb11bcaa94c5b62e778020918922683df
Alexander Abashkin <monolithed@gmail.com>
2016-11-11 03:39:14 +0300

    Fixed tests

--- utils/build.sh
+++ utils/build.sh
@@ -1,12 +1,11 @@
 export NODE_ENV=production;
 
-# https://webpack.js.org/api/cli/
-
-npm run webpack -- \
+npm test && \
+	npm run webpack -- \
 	--display-reasons \
 	--optimize-minimize \
 	--progress \
 	--profile \
 	--display-error-details \
 	--config ./tasks/webpack/production.js
-#	&& cat cache/stats.json | npm run analyzer;
+#	 && cat cache/stats.json | npm run analyzer;
